639931080dec57bfb4e84a6b764f0866b9839862
held-m <bodmbod@gmail.com>
2021-07-01 15:04:58 +0200

    codewars

--- codewars/replace-alphabet-pos/src/main.rs
+++ codewars/replace-alphabet-pos/src/main.rs
@@ -4,8 +4,7 @@ use std::vec::IntoIter;
 
 use std::fs;
 use std::io::Read;
-use std::time::SystemTime; 
-
+use std::time::SystemTime;
 
 /// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 ///
@@ -19,10 +18,8 @@ use std::time::SystemTime;
 /// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
 
 fn main() {
-    
     let text = &get_text()[..];
-    
-    
+
     time_exec_fn(alphabet_position_1, text, "alphabet_position_1");
     time_exec_fn(alphabet_position_2, text, "alphabet_position_2");
     time_exec_fn(alphabet_position_3, text, "alphabet_position_3");
@@ -32,12 +29,11 @@ fn alphabet() -> Vec<char> {
     vec![
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
         's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    ] 
+    ]
 }
 
 fn alphabet_hash() -> Zip<IntoIter<char>, Range<i32>> {
-    alphabet().into_iter()
-    .zip(1..27)
+    alphabet().into_iter().zip(1..27)
 }
 
 fn alphabet_position_1(text: &str) -> String {
@@ -53,15 +49,13 @@ fn alphabet_position_1(text: &str) -> String {
 
                 acc
             })
-    })
-    .collect::<String>();
-
+        })
+        .collect::<String>();
 
     check.trim().to_string()
 }
 
-fn alphabet_position_2(text: &str) -> String { 
-
+fn alphabet_position_2(text: &str) -> String {
     let mut result: String = String::new();
 
     for char in text.to_lowercase().chars() {
@@ -74,9 +68,9 @@ fn alphabet_position_2(text: &str) -> String {
     }
 
     result.trim().to_string()
-
 }
 
+// Best solution
 fn alphabet_position_3(text: &str) -> String {
     text.to_lowercase()
         .chars()
@@ -87,11 +81,9 @@ fn alphabet_position_3(text: &str) -> String {
 }
 
 fn time_exec_fn(f: fn(&str) -> String, text: &str, fn_name: &str) -> String {
-    
     let start_fn = SystemTime::now();
-    let result: String = f(text); 
+    let result: String = f(text);
     let end_fn = SystemTime::now();
-
 
     let exec_time = end_fn
         .duration_since(start_fn)
@@ -104,23 +96,21 @@ fn time_exec_fn(f: fn(&str) -> String, text: &str, fn_name: &str) -> String {
 
 fn get_text() -> String {
     let file = fs::File::open("text.txt");
-    
+
     let mut file = match file {
         Ok(file) => file,
-        Err(e) => panic!("Error open file: {}", e)
+        Err(e) => panic!("Error open file: {}", e),
     };
 
     let mut text = String::new();
-    
+
     match file.read_to_string(&mut text) {
-            Ok(_) => &text,
-            Err(err) => panic!("Error read file: {}", err)
+        Ok(_) => &text,
+        Err(err) => panic!("Error read file: {}", err),
     };
 
     text
-    
 }
-
 
 #[test]
 fn returns_expected() {
@@ -132,7 +122,6 @@ fn returns_expected() {
         alphabet_position_1("The narwhal bacons at midnight."),
         "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20".to_string()
     );
-
 
     assert_eq!(
         alphabet_position_2("The sunset sets at twelve o' clock."),
